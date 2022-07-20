9889ec7cefdf80b75447daa917d1036ef2a387ae
jasonkoehn <jasonkoehn04@gmail.com>
2022-07-20 11:30:24 -0700

    Initial Commit

---
+++ Fleet List II/Fleet_List_IIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fleet_List_IIApp.swift
+//  Fleet List II
+//
+//  Created by Jason Koehn on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Fleet_List_IIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
